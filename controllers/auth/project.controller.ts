dd45a0ef4918e138e34cdb256e5fc4b88c7ae63d
Sahil Kumar Jha <striversahil@gmail.com>
2025-02-03 01:00:00 +0530

    Added Project controllers and updated imports

--- controllers/auth/project.controller.ts
+++ controllers/auth/project.controller.ts
@@ -1,3 +1,55 @@
-import ApiResponse from "../../helper/ApiResponse";
+import { Request, Response } from "express";
 import asyncHandler from "../../helper/asyncHandler";
-import { Request, Response, NextFunction } from "express";
+import ApiResponse from "../../helper/ApiResponse";
+import { Project } from "../../models/auth/project.model";
+
+// Get Project Controller Info
+export const ProjectInfo = asyncHandler(async (req: Request, res: Response) => {
+  const ProjectId = req.params.projectId;
+  if (!ProjectId) {
+    return res
+      .status(401)
+      .json(new ApiResponse(401, {}, "Redirecting to login..."));
+  }
+
+  const project = await Project.findOne({ _id: ProjectId });
+
+  if (!project) {
+    return res
+      .status(404)
+      .json(
+        new ApiResponse(404, {}, "Project could not be found try Create it")
+      );
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, project, "Project Found and Authorized"));
+});
+
+// Creation of New Project Controller
+export const newProject = asyncHandler(async (req: Request, res: Response) => {
+  if (!req.user) {
+    return res
+      .status(400)
+      .json(new ApiResponse(400, {}, "User is not authenticated"));
+  }
+
+  const project = await Project.create({
+    user: req.user._id,
+  });
+
+  if (!project) {
+    return res
+      .status(500)
+      .json(
+        new ApiResponse(500, {}, "Project could not be created \n Server Error")
+      );
+  }
+
+  project.save();
+
+  return res.status(200).json({
+    message: "Project Created Successfully 🚀",
+    project: project,
+  });
+});
