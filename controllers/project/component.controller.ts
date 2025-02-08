52d9ea5677e891b6e7fda82b8b3e6e373f38f839
Sahil Kumar Jha <striversahil@gmail.com>
2025-02-08 18:13:58 +0530

    Add component retrieval and deletion functionality; enhance error handling and response messages

--- controllers/project/component.controller.ts
+++ controllers/project/component.controller.ts
@@ -4,7 +4,41 @@ import ApiResponse from "../../helper/ApiResponse";
 import { Component } from "../../models/project/component.model";
 import { Project } from "../../models/auth/project.model";
 
-// Will Be Post Request as require Component Configuration
+// It will return all the components of the project in Array format so to render in Frontend when User fetches this
+
+export const getComponents = asyncHandler(
+  async (req: Request, res: Response) => {
+    const projectId = req.cookies.project_id;
+    if (!projectId) {
+      return res
+        .status(401)
+        .json(
+          new ApiResponse(
+            401,
+            {},
+            "Project does not exist , Try Create it First"
+          )
+        );
+    }
+    const project = await Project.findById(projectId);
+    if (!project) {
+      return res
+        .status(401)
+        .json(
+          new ApiResponse(
+            401,
+            {},
+            "Project could not be found \n Redirecting to login..."
+          )
+        );
+    }
+    return res
+      .status(200)
+      .json(new ApiResponse(200, project.components, "Components Fetched"));
+  }
+);
+
+// Will Be Post Request as require Component Configurations
 
 export const newComponent = asyncHandler(
   async (req: Request, res: Response) => {
@@ -66,5 +100,57 @@ export const newComponent = asyncHandler(
     return res
       .status(200)
       .json(new ApiResponse(200, component, "Component Added"));
+  }
+);
+
+export const deleteComponent = asyncHandler(
+  async (req: Request, res: Response) => {
+    const componentId = req.params.componentId;
+    if (!componentId) {
+      return res
+        .status(401)
+        .json(
+          new ApiResponse(
+            401,
+            {},
+            "Component does not exist , Try Create it First"
+          )
+        );
+    }
+
+    const component = await Component.findByIdAndDelete(componentId);
+
+    if (!component) {
+      return res
+        .status(500)
+        .json(
+          new ApiResponse(
+            500,
+            {},
+            "Component could not be deleted \n Server Error"
+          )
+        );
+    }
+
+    const project = await Project.findById(req.cookies.project_id);
+    if (!project) {
+      return res
+        .status(401)
+        .json(
+          new ApiResponse(
+            401,
+            {},
+            "Project could not be found \n Redirecting to login..."
+          )
+        );
+    }
+    project.components = project.components.filter(
+      (component) => component.toString() !== componentId
+    );
+    project.save();
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, component, "Component Deleted"));
   }
 );
