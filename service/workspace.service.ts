0a45f52d50ae3d3d070a1d6da1b6ab7c86fb4859
Sahil Kumar Jha <striversahil@gmail.com>
2025-02-14 19:28:16 +0530

    Refactor WorkspaceService; add User model import

--- service/workspace.service.ts
+++ service/workspace.service.ts
@@ -2,44 +2,13 @@
  * Workspace Service : It will Assume that You have done all the Validation Checks
  */
 
-import { Workspace } from "../models/workspace/workspace.model";
+import { User } from "@/models/auth/user.model";
+import {
+  Workspace,
+  WorkspaceInterface,
+} from "../models/workspace/workspace.model";
 
 class WorkspaceService {
-  static async createWorkspace(
-    name: string
-  ): Promise<InstanceType<typeof Workspace>> {
-    /**
-     * (Create Workspace) Return : Workspace Object Containing Workspace Details
-     */
-    try {
-      return await Workspace.create(name);
-    } catch (error) {
-      throw new Error(error as string);
-    }
-  }
-
-  static async deleteWorkspace(workspaceId: string): Promise<null> {
-    /**
-     * (Delete Workspace) Return : null
-     */
-    try {
-      return await Workspace.findByIdAndDelete(workspaceId);
-    } catch (error) {
-      throw new Error(error as string);
-    }
-  }
-
-  static async updateWorkspace(workspaceId: string): Promise<any> {
-    /**
-     * (Update Workspace) Return : Workspace Object Containing Workspace Details
-     */
-    try {
-      return await Workspace.findByIdAndUpdate(workspaceId);
-    } catch (error) {
-      throw new Error(error as string);
-    }
-  }
-
   static async getWorkspace(workspaceId: string): Promise<any> {
     /**
      * (Get Workspace By Id) Return : Workspace Object Containing Workspace Details
@@ -50,6 +19,54 @@ class WorkspaceService {
       throw new Error(error as string);
     }
   }
+
+  static async create(userId: string): Promise<WorkspaceInterface | null> {
+    /**
+     * (Create Workspace) Return : Workspace Object Containing Workspace Details
+     */
+    try {
+      const workspace = new Workspace({ user: userId });
+      await workspace.save();
+      const user = await User.findByIdAndUpdate(userId, {
+        $push: { workspaces: workspace._id },
+      });
+      if (!user) return null;
+      return workspace;
+    } catch (error) {
+      throw new Error(error as string);
+    }
+  }
+
+  static async deleteWorkspace(
+    workspaceId: string,
+    userId: string
+  ): Promise<WorkspaceInterface | null> {
+    /**
+     * (Delete Workspace) Return : null
+     */
+    try {
+      const workspace = await Workspace.findByIdAndDelete(workspaceId);
+      if (!workspace) return null;
+      const user = await User.findByIdAndUpdate(userId, {
+        $pull: { workspaces: workspaceId },
+      });
+      if (!user) return null;
+      return workspace;
+    } catch (error) {
+      throw new Error(error as string);
+    }
+  }
+
+  // static async updateWorkspace(workspaceId: string): Promise<any> {
+  //   /**
+  //    * (Update Workspace) Return : Workspace Object Containing Workspace Details
+  //    */
+  //   try {
+  //     return await Workspace.findByIdAndUpdate(workspaceId);
+  //   } catch (error) {
+  //     throw new Error(error as string);
+  //   }
+  // }
 }
 
 export default WorkspaceService;
